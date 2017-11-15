85173ed973ec0b44e2d5cccb248896da21ec73d4
Mauricio Martinez <n_othing@hotmail.com>
2017-11-15 12:16:59 -0600

    change api key

--- src/app/shared/navbar/navbar.module.ts
+++ src/app/shared/navbar/navbar.module.ts
@@ -2,9 +2,9 @@ import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { RouterModule } from '@angular/router';
 import { NavbarComponent } from './navbar.component';
-
+import { MatSelectModule } from '@angular/material';
 @NgModule({
-    imports: [ RouterModule, CommonModule ],
+    imports: [ RouterModule, CommonModule, MatSelectModule],
     declarations: [ NavbarComponent ],
     exports: [ NavbarComponent ]
 })
