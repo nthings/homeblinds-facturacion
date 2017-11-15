41871f55e6bdeab916affecc0c26f0e5f37975a6
Mauricio Martinez <n_othing@hotmail.com>
2017-11-15 15:42:11 -0600

    nav bar

--- src/app/sidebar/sidebar.module.ts
+++ src/app/sidebar/sidebar.module.ts
@@ -2,9 +2,10 @@ import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { RouterModule } from '@angular/router';
 import { SidebarComponent } from './sidebar.component';
+import { MatSelectModule } from '@angular/material';
 
 @NgModule({
-    imports: [ RouterModule, CommonModule ],
+    imports: [ RouterModule, CommonModule, MatSelectModule ],
     declarations: [ SidebarComponent ],
     exports: [ SidebarComponent ]
 })
