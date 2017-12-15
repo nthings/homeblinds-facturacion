e8b1230967e0793144e477ad086ce57fdd4ee233
Mauricio Martinez <n_othing@hotmail.com>
2017-12-15 13:58:52 -0600

    Validate RFC

--- src/app/dialogs/client-dialog/client-dialog.component.ts
+++ src/app/dialogs/client-dialog/client-dialog.component.ts
@@ -48,6 +48,17 @@ export class ClientDialogComponent implements OnInit {
             this.clientForm.controls['address'].get('neighborhood').setValue(this.data.address.neighborhood, {onlySelf: true});
             this.clientForm.controls['address'].get('zip').setValue(this.data.address.zip, {onlySelf: true});
         }
+        // Validar RFC
+        this.clientForm.controls['tax_id'].valueChanges.subscribe(
+            rfc => {
+                if (!this.rfcValido(rfc)) {
+                    this.clientForm.controls['tax_id'].setErrors({rfcInvalid: true});
+                } else {
+                    // RFC Valido
+                    this.clientForm.controls['tax_id'].setErrors(null);
+                }
+            }
+        );
     }
 
     onNoClick(): void {
@@ -87,4 +98,49 @@ export class ClientDialogComponent implements OnInit {
             );
         }
     }
+
+    rfcValido(rfc) {
+        const re = /^([A-ZÑ&]{3,4}) ?(?:- ?)?(\d{2}(?:0[1-9]|1[0-2])(?:0[1-9]|[12]\d|3[01])) ?(?:- ?)?([A-Z\d]{2})([A\d])$/;
+        const validado = rfc.match(re);
+
+        if (!validado) {  // Coincide con el formato general del regex?
+            return false;
+        }
+
+        // Separar el dígito verificador del resto del RFC
+        const digitoVerificador = validado.pop();
+        const rfcSinDigito = validado.slice(1).join('');
+        const len = rfcSinDigito.length;
+        const diccionario = '0123456789ABCDEFGHIJKLMN&OPQRSTUVWXYZ Ñ';
+        const indice = len + 1;
+        let suma;
+        let digitoEsperado;
+
+        if (len === 12) {
+            suma = 0;
+        } else {
+            suma = 481; // Ajuste para persona moral
+        }
+
+        for (let i = 0; i < len; i++) {
+            suma += diccionario.indexOf(rfcSinDigito.charAt(i)) * (indice - i);
+        }
+
+        digitoEsperado = 11 - suma % 11;
+        if (digitoEsperado === 11) {
+            digitoEsperado = 0;
+        } else if (digitoEsperado === 10) {
+            digitoEsperado = 'A';
+        }
+        // El dígito verificador coincide con el esperado?
+        // o es un RFC Genérico (ventas a público general)?
+        if ((digitoVerificador !== digitoEsperado)
+            && (rfcSinDigito + digitoVerificador !== 'XAXX010101000')) {
+            return false;
+        } else if (rfcSinDigito + digitoVerificador === 'XEXX010101000') {
+            return false;
+        }
+        return rfcSinDigito + digitoVerificador;
+    }
+
 }
